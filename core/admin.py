f63b2e6bf3b1e71d4922a48160208936f3288c74
mariyasmin <mari.yasminnnn@gmail.com>
2022-07-07 16:50:17 -0300

    adding new models

--- core/admin.py
+++ core/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from core.models import Autor,Editora, Categoria 
+from core.models import Autor, Editora, Categoria, Livro
 
 admin.site.register(Categoria)
 admin.site.register(Editora)
 admin.site.register(Autor)
+admin.site.register(Livro)
--- core/models.py
+++ core/models.py
@@ -1,4 +1,6 @@
+from tabnanny import verbose
 from django.db import models
+
 
 class Categoria(models.Model):
     descricao = models.CharField(max_length=255)
@@ -6,12 +8,14 @@ class Categoria(models.Model):
     def __str__(self):
         return self.descricao
 
+
 class Editora(models.Model):
     nome = models.CharField(max_length=255)
     site = models.URLField()
 
     def __str__(self):
-        return self.nome 
+        return self.nome
+
 
 class Autor(models.Model):
     nome = models.CharField(max_length=255)
@@ -19,3 +23,13 @@ class Autor(models.Model):
 
     def __str__(self):
         return self.nome
+
+
+class Livro(models.Model):
+    titulo = models.CharField(max_length=255)
+    isbn = models.CharField(max_length=32)
+    quantidade = models.IntegerField()
+    preco = models.DecimalField(max_digits=7, decimal_places=2)
+    categoria = models.ForeignKey(
+        Categoria, on_delete=models.PROTECT, related_name="livros")
+    editora = models.ForeignKey(Editora, on_delete=models.PROTECT, related_name="livros")
