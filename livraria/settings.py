bb607b3d1c078346b1c25a9bd8987cb313be9b7c
mariyasmin <mari.yasminnnn@gmail.com>
2022-09-26 14:30:17 -0300

    using DRF permissions

--- livraria/settings.py
+++ livraria/settings.py
@@ -131,3 +131,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOW_ALL_ORIGINS = True
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 STATIC_ROOT = os.path.join(BASE_DIR, 'static') 
+
+
+REST_FRAMEWORK = {
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.DjangoModelPermissions",
+    ]
+}
