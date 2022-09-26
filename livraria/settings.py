1a1348130c9251d70e8ff0cef391d35f7099af88
mariyasmin <mari.yasminnnn@gmail.com>
2022-09-26 15:36:01 -0300

    using simpleJWT for authentication on DRF

--- livraria/settings.py
+++ livraria/settings.py
@@ -136,5 +136,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.DjangoModelPermissions",
-    ]
+    
+    ],
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication", )
 }
