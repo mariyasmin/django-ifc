21741cf5f42115335391a4d00106648853993b9f
mariyasmin <mari.yasminnnn@gmail.com>
2022-11-21 14:25:39 -0300

    21- dump and data load

--- core/models/livro.py
+++ core/models/livro.py
@@ -6,9 +6,9 @@ from media.models import Image
 
 class Livro(models.Model):
     titulo = models.CharField(max_length=255)
-    isbn = models.CharField(max_length=32)
-    quantidade = models.IntegerField()
-    preco = models.DecimalField(max_digits=7, decimal_places=2)
+    isbn = models.CharField(max_length=32, blank=True, null=True)
+    quantidade = models.IntegerField(blank=True, null=True)
+    preco = models.DecimalField(max_digits=7, decimal_places=2, blank=True, null=True)
     categoria = models.ForeignKey(
         Categoria, on_delete=models.PROTECT, related_name="livros")
     editora = models.ForeignKey(Editora, on_delete=models.PROTECT, related_name="livros")
