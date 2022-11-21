b8bfa9aca980bb16ba219576bbfb7057c296cf33
mariyasmin <mari.yasminnnn@gmail.com>
2022-11-21 14:44:26 -0300

    23 -admin configuration

--- core/admin.py
+++ core/admin.py
@@ -4,15 +4,71 @@ from django.utils.translation import gettext_lazy as _
 
 from core.models import Autor, Categoria, Editora, Livro, Usuario
 
-admin.site.register(Autor)
-admin.site.register(Categoria)
-admin.site.register(Editora)
-admin.site.register(Livro)
+# from faker import Faker
 
+
+
+@admin.register(Autor)
+class AutorAdmin(admin.ModelAdmin):
+    list_display = ("nome", "email")
+    search_fields = ("nome", "email")
+    list_filter = ("nome",)
+    ordering = ("nome", "email")
+    actions = ['gerar_autores']
+
+    # def gerar_autores(self, request, queryset):
+    #     faker = Faker()
+    #     for _ in range(10):
+    #         Autor.objects.create(
+    #             nome=faker.name(),
+    #             email=faker.email(),
+    #         )
+    #     self.message_user(request, 'Autores gerados com sucesso!')
+    # gerar_autores.short_description = 'Gerar autores'
+
+@admin.register(Categoria)
+class CategoriaAdmin(admin.ModelAdmin):
+    list_display = ("descricao",)
+    search_fields = ("descricao",)
+    list_filter = ("descricao",)
+    ordering = ("descricao",)
+
+
+@admin.register(Editora)
+class EditoraAdmin(admin.ModelAdmin):
+    list_display = ("nome",)
+    search_fields = ("nome",)
+    list_filter = ("nome",)
+    ordering = ("nome",)
+
+
+@admin.register(Livro)
+class LivroAdmin(admin.ModelAdmin):
+    list_display = ("titulo", "editora", "categoria")
+    search_fields = ("titulo", "editora__nome", "categoria__descricao")
+    list_filter = ("editora", "categoria")
+    ordering = ("titulo", "editora", "categoria")
+    list_per_page = 25
+
+
+@admin.register(Usuario)
 class UsuarioAdmin(UserAdmin):
     fieldsets = (
         (None, {"fields": ("username", "password")}),
-        (_("Personal info"), {"fields": ("first_name", "last_name", "email", "cpf", "telefone", "data_nascimento")}),
+        (
+            _("Personal info"),
+            {
+                "fields": (
+                    "first_name",
+                    "last_name",
+                    "foto",
+                    "email",
+                    "cpf",
+                    "telefone",
+                    "data_nascimento",
+                )
+            },
+        ),
         (
             _("Permissions"),
             {
@@ -27,5 +83,3 @@ class UsuarioAdmin(UserAdmin):
         ),
         (_("Important dates"), {"fields": ("last_login", "date_joined")}),
     )
-
-admin.site.register(Usuario, UsuarioAdmin)
